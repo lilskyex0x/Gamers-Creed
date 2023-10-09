314bcfabed8db26a5820e560d184391cbb6dc14a
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-10-10 02:56:25 +0630

    Fetched function and successfuly logged the data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Gamers' Creed</h1>
+      <Home/>
     </div>
   );
 }
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './Slices/HomeSlice'
+import homeReducer from './Slices/HomeSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    home: homeReducer,
   },
 })
