93755f570cf2020fbde539ffb1056978e019b44a
unknown <lilskyex0x.apk@gmail.com>
2023-10-12 19:55:52 +0630

    Updated styling for components

--- src/components/gameItems/GameItems.jsx
+++ src/components/gameItems/GameItems.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchGamesByTitle } from "../../redux/Slices/gameSlice";
+import { BsArrowRightCircle } from "react-icons/bs";
 import '../styles/GameItems.css';
 
 function GameItems({ searchQuery }) {
@@ -10,8 +11,8 @@ function GameItems({ searchQuery }) {
 
   const games = searchQuery
     ? allGames.filter((game) =>
-        game.name.toLowerCase().includes(searchQuery.toLowerCase())
-      )
+      game.name.toLowerCase().includes(searchQuery.toLowerCase())
+    )
     : allGames;
 
   useEffect(() => {
@@ -30,11 +31,12 @@ function GameItems({ searchQuery }) {
             </div>
             <div className="content__container">
               <h2 className="game__title">{game.name}</h2>
+              <span className="line__through"><BsArrowRightCircle /></span>
               <div className="about__game">
-                <p className="game__rating">Rating {game.rating}</p>
+                <p className="game__rating">{game.rating} <span>- Rating</span></p>
                 <p className="game__textRating">{game.textRating}</p>
-                <p className="game__Nprice">Normal Price - {game.normalPrice}</p>
-                <p className="game__Sprice">Sale Price - {game.salePrice}</p>
+                <p className="game__Nprice">${game.normalPrice} - Normal Price</p>
+                <p className="game__Sprice">${game.salePrice} - Sale Price</p>
               </div>
             </div>
           </Link>
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -17,63 +17,62 @@ function Detail() {
   }, [dispatch, id]);
 
   return (
-    <div>
+    <>
       <Navbar data="Game Details" />
-      <main>
-        <div>
-          {gameDetail ? (
-            <div>
-              <h2>{gameDetail.info.title}</h2>
-              <img src={gameDetail.info.thumb} alt="" />
-              <p>
-                Cheapest price right now: ${gameDetail.cheapestPriceEver.price}
-              </p>
-              {gameData ? (
-                <div>
-                  {gameData.find((game) => game.id === id) ? (
-                    gameData.map((game) => {
-                      if (game.id === id) {
-                        return (
-                          <div key={game.id}>
-                            <p>Rating: {game.rating}</p>
-                            <p>Text Rating: {game.textRating}</p>
-                            <p>Metacritic Score: {game.score}</p>
-                            <p>Normal Price: {game.normalPrice}</p>
-                            <p>Sale Price: {game.salePrice}</p>
-                          </div>
-                        );
-                      }
-                      return null;
-                    })
-                  ) : (
-                    <div>No avaliable game data from API...</div>
-                  )}
-                </div>
-              ) : (
-                <div>Loading game data...</div>
-              )}
-              {gameDetail.deals && gameDetail.deals.length > 0 ? (
-                <div>
-                  <h2>Deals Available!</h2>
-                  {gameDetail.deals.slice(0, 3).map((deal, index) => (
-                    <div className="deal" key={index}>
-                      <h3>Deal {index + 1}</h3>
-                      <p>Deal price: {deal.price}</p>
-                      <p>Retail Price: ${deal.retailPrice}</p>
-                      <p>You will save: ${deal.savings}</p>
-                    </div>
-                  ))}
-                </div>
-              ) : (
-                <p>No deals available</p>
-              )}
-            </div>
-          ) : (
-            <div>Loading...</div>
-          )}
-        </div>
+      <main className="detail__container">
+        {gameDetail ? (
+          <div className="detail__contents">
+            <h2>{gameDetail.info.title}</h2>
+            <img src={gameDetail.info.thumb} alt="" />
+            <p>
+              Cheapest price right now: ${gameDetail.cheapestPriceEver.price}
+            </p>
+            {gameData ? (
+              <div>
+                {gameData.find((game) => game.id === id) ? (
+                  gameData.map((game) => {
+                    if (game.id === id) {
+                      return (
+                        <div key={game.id}>
+                          <p>Rating: {game.rating}</p>
+                          <p>Text Rating: {game.textRating}</p>
+                          <p>Metacritic Score: {game.score}</p>
+                          <p>Normal Price: {game.normalPrice}</p>
+                          <p>Sale Price: {game.salePrice}</p>
+                        </div>
+                      );
+                    }
+                    return null;
+                  })
+                ) : (
+                  <div>No available game data from API...</div>
+                )}
+              </div>
+            ) : (
+              <div className="loading">Loading game data...</div>
+            )}
+            {gameDetail.deals && gameDetail.deals.length > 0 ? (
+              <div className="deals">
+                <h2>Deals Available!</h2>
+                {gameDetail.deals.slice(0, 3).map((deal, index) => (
+                  <div className="deal" key={index}>
+                    <h3>Deal {index + 1}</h3>
+                    <p>Deal price: {deal.price}</p>
+                    <p>Retail Price: ${deal.retailPrice}</p>
+                    <p>You will save: ${deal.savings}</p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p className="no-deals">No deals available</p>
+            )}
+          </div>
+        ) : (
+          <div className="loading">Loading...</div>
+        )}
       </main>
-    </div>
+
+    </>
   );
 }
 
