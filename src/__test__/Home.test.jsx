252af73582511153b34afb188830d936acc7d766
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-10-13 23:20:38 +0630

    fix linter errors

--- src/__test__/Home.test.jsx
+++ src/__test__/Home.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
